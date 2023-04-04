563c9c58bfca79a668007be6c94b09675428d3c6
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-04 22:44:38 +0530

    Update authentications.middlewares.js

--- Middlewares/authentications.middlewares.js
+++ Middlewares/authentications.middlewares.js
@@ -1,10 +1,11 @@
 const jwt=require("jsonwebtoken")
 require('dotenv').config();
+const secretKey = process.env.key;
 
 const authenticate=(req, res, next) => {
     const token=req.headers.authorization || null
     if(token){
-        const decoded=jwt.verify(token, prossece.env.key)
+        const decoded=jwt.verify(token,secretKey)
         if(decoded){
             const userID=decoded.userID
             console.log(decoded)
