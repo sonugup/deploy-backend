928aa0e6107081cb6260ab2f854d45810a722e12
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-04 22:54:09 +0530

    Update Note.route.js

--- routes/Note.route.js
+++ routes/Note.route.js
@@ -38,7 +38,7 @@ notesRouter.patch("/updated/:id", async (req, res) => {
   const id=req.params.id
   const note=await NoteModel.findOne({"_id":id})
   const userId_note=note.userId
-  const userID_making=req.body.userId;
+  const userID_making=req.body.userID;
   try{
     if(userID_making === userId_note){
       res.send({"msg":"You are not authorized"})
