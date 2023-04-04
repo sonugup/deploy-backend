80ed0ab93395d72eba86f8d1e41d822dbca6bc2f
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-04 22:38:42 +0530

    Update authentications.middlewares.js

--- Middlewares/authentications.middlewares.js
+++ Middlewares/authentications.middlewares.js
@@ -3,7 +3,7 @@ const jwt=require("jsonwebtoken")
 const authenticate=(req, res, next) => {
     const token=req.headers.authorization
     if(token){
-        const decoded=jwt.verify(token, "sonu")
+        const decoded=jwt.verify(token, prossece.env.key)
         if(decoded){
             const userID=decoded.userID
             console.log(decoded)
